2fe9b035d22eb98806ae04357e700d9ddc737dea
fanur <makhiyanov.ff@gmail.com>
2022-06-28 17:19:56 +0300

    Task01_S7

---
+++ Task01/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// // m = 3, n = 4.
+// 0,5  7    -2   -0,2
+// 1   -3,3   8   -9,9
+// 8    7,8  -7,1  9
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] GenerateArray(int row, int column, int min, int max)
+{
+    int array = new int[row, column];
+    int rnd = new Random();
+    for (int i = 0; i < array.GetLenght(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = Prompt("Количество строк -> ");
+int column = Prompt("Количество столбцов -> ");
+int min = -10;
+int max = 10;
+int[,] array = GenerateArray(row, column, min, max);
+PrintArray(array);
