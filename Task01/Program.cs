924babd9f11ef4fff98d34f5efa36168ea85af78
fanur <makhiyanov.ff@gmail.com>
2022-07-01 09:28:41 +0300

    Task01_S7

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -10,36 +10,33 @@ int Prompt(string message)
     return int.Parse(Console.ReadLine());
 }
 
-int[,] GenerateArray(int row, int column, int min, int max)
+double[,] GenerateArray(int row, int column)
 {
-    int array = new int[row, column];
-    int rnd = new Random();
-    for (int i = 0; i < array.GetLenght(0); i++)
+    var array = new double[row, column];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = rnd.Next(min, max + 1);
+            array[i, j] = rnd.NextDouble() * 100;
         }
     }
-
     return array;
 }
 
-void PrintArray(int[,] array)
+void PrintArray(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            System.Console.Write(array[i, j] + "\t");
+            Console.Write($"{array[i, j]:F2}" + "\t");
         }
-        System.Console.WriteLine();
+        Console.WriteLine();
     }
 }
 
 int row = Prompt("Количество строк -> ");
 int column = Prompt("Количество столбцов -> ");
-int min = -10;
-int max = 10;
-int[,] array = GenerateArray(row, column, min, max);
+double[,] array = GenerateArray(row, column);
 PrintArray(array);
